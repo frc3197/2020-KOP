86d8c3ddb8db019b84d6dc6e4d15e4af95382b7f
SeanLDubs <seanlightwhite@gmail.com>
2020-02-06 19:44:44 -0600

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
-import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.commands.Drive;
 
